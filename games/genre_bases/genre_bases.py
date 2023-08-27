57eb44bb974e4aec42c7cc8716f3fd51ffa2b0f1
Gordon Lo <lochengxin@gmail.com>
2023-08-28 03:11:30 +1200

    finish the search function

--- games/genre_bases/genre_bases.py
+++ games/genre_bases/genre_bases.py
@@ -8,47 +8,8 @@ genre_bases_blueprint = Blueprint("genre_bases_bp", __name__)
 
 games_per_page = 30
 
-
-
-#@genre_bases_blueprint.route('/genre/<target>', methods=["GET"])
-def show_games_trial(target):
-    pagenum = request.args.get("page")
-    order = request.args.get("order")
-    
-    if not order:
-        order =""
-    
-    #if pagenum is not set then page is 1 else page is given value for invalid page
-    if not pagenum:
-        pagenum = 1
-    else:
-        try:
-            pagenum = int(pagenum)
-        except:
-            return render_template("notFound.html", message=f"Invalid page value!")
-        
-    
-    num_games = services.get_number_of_games(repo.repo_instance)
-    games = services.get_games(repo.repo_instance, games_per_page, pagenum, order)
-    maxpage = services.get_max_page_num(num_games, games_per_page)
-    pages = services.generate_page_list(pagenum, maxpage)
-    option_of_order = ["game_id", "title", "publisher", "release_date", "price"]
-    geners_list = services.get_genre_list(repo.repo_instance)
-    publisher_list = services.get_publisher_list(repo.repo_instance)
-    
-    
-    page_info = {
-        "number_of_games": num_games,
-        "maxpage": maxpage,
-        "current_display": services.get_current_display(num_games, games_per_page, pagenum),
-        "current_page": pagenum,
-        "current_order": order
-    }
-    
-    return render_template("games.html", games=games, num_game=num_games, page_info=page_info, pages=pages, order_options=option_of_order,genres=geners_list,publishers=publisher_list)
-
 @genre_bases_blueprint.route('/genre/<target>', methods=["GET"])
-def show_games(target):
+def show_games_trial(target):
     ##
     pagenum = request.args.get("page")
     order = request.args.get("order")
--- games/search/search.py
+++ games/search/search.py
@@ -8,44 +8,8 @@ games_per_page = 30
 
 
 
-#@search_blueprint.route('/search/<target>/<type>', methods=["GET"])
-def show_games(target,type):
-    pagenum = request.args.get("page")
-    order = request.args.get("order")
-    
-    if not order:
-        order =""
-    
-    #if pagenum is not set then page is 1 else page is given value for invalid page
-    if not pagenum:
-        pagenum = 1
-    else:
-        try:
-            pagenum = int(pagenum)
-        except:
-            return render_template("notFound.html", message=f"Invalid page value!")
-        
-    
-    num_games = services.get_number_of_games(repo.repo_instance)
-    games = services.get_games(repo.repo_instance, games_per_page, pagenum, order)
-    maxpage = services.get_max_page_num(num_games, games_per_page)
-    pages = services.generate_page_list(pagenum, maxpage)
-    option_of_order = ["game_id", "title", "publisher", "release_date", "price"]
-    geners_list = services.get_genre_list(repo.repo_instance)
-    publisher_list = services.get_publisher_list(repo.repo_instance)
-    
-    page_info = {
-        "number_of_games": num_games,
-        "maxpage": maxpage,
-        "current_display": services.get_current_display(num_games, games_per_page, pagenum),
-        "current_page": pagenum,
-        "current_order": order
-    }
-    
-    return render_template("games.html", games=games, num_game=num_games, page_info=page_info, pages=pages, order_options=option_of_order,genres=geners_list,publishers=publisher_list)
-
 @search_blueprint.route('/search/<target>/<type>', methods=["GET"])
-def show_games_trial(target,type):
+def show_games(target,type):
     ##
     pagenum = request.args.get("page")
     order = request.args.get("order")
@@ -95,6 +59,6 @@ def show_games_trial(target,type):
     
     print(games)
     
-    #return render_template("search.html", games=games, num_game=num_games, page_info=page_info, pages=pages, order_options=option_of_order,genres=genres_list,publishers=publisher_list,type=type,target=target)
-    return render_template("genre.html", games=games, num_game=num_games, page_info=page_info, pages=pages, order_options=option_of_order,genres=genres_list,publishers=publisher_list,genre=target)
+    return render_template("search.html", games=games, num_game=num_games, page_info=page_info, pages=pages, order_options=option_of_order,genres=genres_list,publishers=publisher_list,type=type,target=target)
+    #return render_template("genre.html", games=games, num_game=num_games, page_info=page_info, pages=pages, order_options=option_of_order,genres=genres_list,publishers=publisher_list,genre=target)
 
