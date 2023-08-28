8c074a9a930436f4b23b6f62f8b151f1931574cc
Kingsley Leung <kingsleyleung2003@outlook.com>
2023-08-28 16:57:20 +1200

    Modify the way of verify test_repo_can_add_a_game

--- tests/unit/test_memoryrepository.py
+++ tests/unit/test_memoryrepository.py
@@ -9,7 +9,7 @@ def test_repo_can_add_a_game(in_memory_repo):
     game = Game(897220, 'Summer Pockets')
     in_memory_repo.add_game(game)
 
-    assert in_memory_repo.get_number_of_games() == game_num_og + 1
+    assert in_memory_repo.get_game_by_id(897220) is game
 
 def test_repo_can_retrieve_a_game(in_memory_repo):
     game = in_memory_repo.get_game_by_id(1002510)
