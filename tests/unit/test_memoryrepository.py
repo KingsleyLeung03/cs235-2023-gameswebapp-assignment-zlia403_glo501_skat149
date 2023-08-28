4d313b43a6e344d0b57cc20d38485dbd0f594837
Kingsley Leung <kingsleyleung2003@outlook.com>
2023-08-28 17:13:48 +1200

    Implement test_memoryrepository.py except for the last two requirements related to searching

--- tests/unit/test_memoryrepository.py
+++ tests/unit/test_memoryrepository.py
@@ -1,11 +1,9 @@
 import pytest
 
-from games.domainmodel.model import Game
+from games.domainmodel.model import Game, Genre
 
 
 def test_repo_can_add_a_game(in_memory_repo):
-    game_num_og = in_memory_repo.get_number_of_games()
-
     game = Game(897220, 'Summer Pockets')
     in_memory_repo.add_game(game)
 
@@ -15,3 +13,27 @@ def test_repo_can_retrieve_a_game(in_memory_repo):
     game = in_memory_repo.get_game_by_id(1002510)
 
     assert game == Game(1002510, "The Spell - A Kinetic Novel")
+
+def test_repo_retrieves_correct_num_of_game(in_memory_repo):
+    game_num = in_memory_repo.get_number_of_games()
+
+    assert game_num == 877
+
+def test_repo_num_of_unique_genre(in_memory_repo):
+    genre_num = in_memory_repo.get_number_of_genres()
+
+    assert genre_num == 24
+
+def test_repo_add_new_genre_and_increase_count(in_memory_repo):
+    # Get the origin num of genres
+    genre_num = in_memory_repo.get_number_of_genres()
+
+    genre = Genre("Visual Novel")
+    in_memory_repo.add_genre(genre)
+
+    # Check if the new genre is inside the all genre list
+    assert genre in in_memory_repo.get_genre_list()
+
+    # Check if the num of genres has been increased by 1
+    assert genre_num + 1 == in_memory_repo.get_number_of_genres()
+
