a6cb7962aee6bb3e53b7c71b6527ae88f2083b29
Kingsley Leung <kingsleyleung2003@outlook.com>
2023-08-28 16:28:10 +1200

    Implement the basic of memory repo test code

---
+++ tests/unit/test_memoryrepository.py
@@ -0,0 +1,17 @@
+import pytest
+
+from games.domainmodel.model import Game
+
+
+def test_repo_can_add_a_game(in_memory_repo):
+    game_num_og = in_memory_repo.get_number_of_games()
+
+    game = Game(897220, 'Summer Pockets')
+    in_memory_repo.add_game(game)
+
+    assert in_memory_repo.get_number_of_games() == game_num_og + 1
+
+def test_repo_can_retrieve_a_game(in_memory_repo):
+    game = in_memory_repo.get_game_by_id(1002510)
+
+    assert game == Game(1002510, "The Spell - A Kinetic Novel")
