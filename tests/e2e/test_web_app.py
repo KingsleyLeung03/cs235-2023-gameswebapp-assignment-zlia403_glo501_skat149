8e62c64cf61e88bfaf71f51355a99d3942522086
Kingsley Leung <kingsleyleung2003@outlook.com>
2023-09-17 12:51:35 +1200

    Create the basic structure of e2e test

---
+++ tests/e2e/test_web_app.py
@@ -0,0 +1,25 @@
+import pytest
+
+from flask import session
+
+# Test registering
+
+
+
+
+# Test logging in
+
+
+
+
+# Test browsing games
+
+
+
+
+# Test adding games to the favourite list
+
+
+
+
+# Test logging out
