348b415dc8b98ec928584b32571543e6981e158c
Kingsley Leung <kingsleyleung2003@outlook.com>
2023-09-18 12:35:29 +1200

    Implement games page with order E2E test, not yet finish

--- tests/e2e/test_web_app.py
+++ tests/e2e/test_web_app.py
@@ -50,8 +50,39 @@ def test_e2e_games_page_with_page_num(client):
     assert b'Hexodius' in response_page_2.data
     assert b'Expeditions: Conquistador' in response_page_2.data
 
-# def test_e2e_games_page_with_order(client):
+def test_e2e_games_page_with_order(client):
     # Check that we can retrieve the games page with order.
+    response_game_id = client.get('/games?page=2&order=game_id')
+    response_title = client.get('/games?page=2&order=title')
+    response_publisher = client.get('/games?page=2&order=publisher')
+    response_release_date = client.get('/games?page=2&order=release_date')
+    response_price = client.get('/games?page=2&order=price')
+    assert response_game_id.status_code == 200
+    assert response_title.status_code == 200
+    assert response_publisher.status_code == 200
+    assert response_release_date.status_code == 200
+    assert response_price.status_code == 200
+
+    # Check that the first 3 games we need are included on the page.
+    assert b'Deadfall Adventures' in response_game_id.data
+    assert b'Hexodius' in response_game_id.data
+    assert b'Expeditions: Conquistador' in response_game_id.data
+
+    assert b'Alien Planet' in response_title.data
+    assert b'Aliens vs. Ghosts' in response_title.data
+    assert b'Alone In The Mars' in response_title.data
+
+    assert b'Into the Pyramid' in response_publisher.data
+    assert b"Gripper's Adventure" in response_publisher.data
+    assert b'SC2KRender' in response_publisher.data
+
+    assert b'VELONE' in response_release_date.data
+    assert b'Gem Tower Defense 2' in response_release_date.data
+    assert b"Darza's Dominion" in response_release_date.data
+
+    assert b'MagiCats Builder (Crazy Dreamz)' in response_price.data
+    assert b'Lumberjack VR' in response_price.data
+    assert b'Google Spotlight Stories: Special Delivery' in response_price.data
 
 
 
