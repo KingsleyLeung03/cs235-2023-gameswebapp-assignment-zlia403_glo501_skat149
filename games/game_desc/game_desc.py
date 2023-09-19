2c3d641af0f6e2c56cfdf0c9a83775bb4ba784b2
Gordon Lo <lochengxin@gmail.com>
2023-09-19 14:39:22 +1200

    get the user login function basic done

--- games/game_desc/game_desc.py
+++ games/game_desc/game_desc.py
@@ -61,10 +61,16 @@ def change_favourite():
 @game_desc_blueprint.route('/review/<game_id>/<rate>/<comment>', methods=["GET"])
 def review(game_id: int, rate: int, comment: str):
     user_name = None
-    print(game_id)
-    print(rate)
-    print(comment)
-    print(user_name)
-    services.review(repo.repo_instance,game_id,rate,comment,user_name)
-    return game_description(3010)
+    if "User_name" in session:
+        user_name = session["User_name"]
+        print(game_id)
+        print(rate)
+        print(comment)
+        print(user_name)
+        services.review(repo.repo_instance,game_id,rate,comment,user_name)
+        return game_description(3010)
+    else :
+        print("No user login yet")
+        return game_description(3010)
+
 
