90092460e5f08c5270c49010b66892f707dea9db
Kingsley Leung <kingsleyleung2003@outlook.com>
2023-09-20 22:02:23 +1200

    Add profile to the sidebar, check authentication to adjust profile page's sidebar

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -3,35 +3,39 @@ from games.domainmodel.model import *
 
 import games.authentication.authentication as authentication
 import games.adapters.repository as repo
-import games.profile.services as services
+import games.profile.services as profile_services
+import games.utilities.services as utilities_services
 
 profile_blueprint = Blueprint("profile_bp", __name__)
 
 
 @profile_blueprint.route('/profile', methods=['GET'])
 def show_profile():
-    if not authentication.check_authenticated():
+    authenticated = authentication.check_authenticated()
+    if not authenticated:
         return redirect(url_for('home_bp.home'))
     user_name = session["User_name"]
 
     # Use the testing profile for testing
     # profile = services.get_profile(repo.repo_instance, "demo_user")
 
-    profile = services.get_profile(repo.repo_instance, user_name)
+    profile = profile_services.get_profile(repo.repo_instance, user_name)
+    genre_list = utilities_services.get_genre_list(repo.repo_instance)
 
-    return render_template("profile.html", profile=profile)
+    return render_template("profile.html", profile=profile, genres=genre_list, authenticated=authenticated)
 
 
 @profile_blueprint.route('/profile/remove_favourite/<game_id>', methods=['GET'])
 def remove_favourite(game_id):
-    if not authentication.check_authenticated():
+    authenticated = authentication.check_authenticated()
+    if not authenticated:
         return redirect(url_for('home_bp.home'))
     user_name = session["User_name"]
 
     # Use the testing profile for testing
     # services.remove_favourite(repo.repo_instance, "demo_user", int(game_id))
 
-    services.remove_favourite(repo.repo_instance, user_name, int(game_id))
+    profile_services.remove_favourite(repo.repo_instance, user_name, int(game_id))
 
     return redirect(url_for("profile_bp.show_profile"))
 
